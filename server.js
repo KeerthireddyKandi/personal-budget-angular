9ef8adaf740ba33dd8696f8b417c42d74db696a6
Keerthi Reddy <keerthireddy@Keerthis-MacBook-Pro.local>
2020-10-08 11:18:09 -0400

    CORS DONE

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express= require('express');
+const cors= require('cors');
 const app= express();
 const port= 3000;
 
-app.use('/', express.static('public'));
+app.use(cors());
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World from Express!');
-  });
-  
+/*const budget=
+{
+  "myBudget": [
+      {
+          "title": "Eat Out",
+          "budget": 30
+      },
+      {
+          "title": "Rent",
+          "budget": 100
+      },
+      {
+          "title": "Groceries",
+          "budget": 90
+      },
+    ]
+  };
+*/
  app.get('/budget' , (req, res) => {
      const file= require('fs');
      const dataobj=file.readFileSync('new.json');
@@ -16,5 +31,5 @@ app.get('/hello', (req, res) => {
   });
 
   app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`API served at http://localhost:${port}`)
 });
